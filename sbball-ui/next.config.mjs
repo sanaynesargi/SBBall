a1a8cee27c33c0591f413400bbee80604fce7043
Sanay Nesargi <sanaynesargi@gmail.com>
2024-05-04 12:32:26 -0500

    T

--- sbball-ui/next.config.mjs
+++ sbball-ui/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  experimental: {
+    outputFileTracingIgnores: [
+      // builds on vercel used to ignore these dependencies before but not anymore, so builds are failing
+      // TODO remove these once builds are fixed in vercel
+      "**/node_modules/@swc/core-linux-x64-gnu",
+      "**/node_modules/@swc/core-linux-x64-musl",
+      "**/node_modules/@esbuild/linux-x64",
+      "**/node_modules/terser/dist",
+      "**/node_modules/uglify-js/lib",
+    ],
+  },
+};
 
 export default nextConfig;
