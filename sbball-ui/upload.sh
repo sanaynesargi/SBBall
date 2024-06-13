90ac594af316852cc52c39c0f91c488248de692d
Sanay Nesargi <sanaynesargi@gmail.com>
2024-06-13 09:58:00 +0200

    Changed Out

--- sbball-ui/upload.sh
+++ sbball-ui/upload.sh
@@ -1,2 +1,2 @@
 yarn build
-rsync -avz --info=progress2 -e  "ssh -i ~/.ssh/fm-server.pem" .next/ ubuntu@ec2-3-86-198-164.compute-1.amazonaws.com:/home/ubuntu/SBBall/sbball-ui/.next
+rsync -avz --info=progress2 -e  "ssh -i ~/.ssh/fm-server.pem" .next/ ubuntu@ec2-54-144-179-206.compute-1.amazonaws.com:/home/ubuntu/SBBall/sbball-ui/.next
--- sbball-ui/utils/apiUrl.tsx
+++ sbball-ui/utils/apiUrl.tsx
@@ -1,2 +1,4 @@
 export const apiUrl =
-  process.env.NODE_ENV == "development" ? "localhost:8080" : "44.226.227.51";
+  process.env.NODE_ENV == "development"
+    ? "localhost:8080"
+    : "http://54.144.179.206/bball/";
