bf6225ece9f93ca8eebd69b07402b9594630572a
Sanay Nesargi <sanaynesargi@gmail.com>
2025-11-08 11:08:39 -0600

    feat: enable dark mode by default

--- sbball-ui/src/app/providers.tsx
+++ sbball-ui/src/app/providers.tsx
@@ -1,10 +1,16 @@
 // app/providers.tsx
 "use client";
 
-import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme, type ThemeConfig } from "@chakra-ui/react";
 import { BottomNavigationStyleConfig } from "chakra-ui-bottom-navigation";
 
+const config: ThemeConfig = {
+  initialColorMode: 'dark',
+  useSystemColorMode: false,
+};
+
 const theme = extendTheme({
+  config,
   components: {
     BottomNavigation: BottomNavigationStyleConfig,
   },
